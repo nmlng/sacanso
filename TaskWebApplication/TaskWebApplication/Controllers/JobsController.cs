e33a6f187905f26f9fa642d687732f9f3c20d74c
Nuno Guerreiro <nunomguerreiro@gmail.com>
2016-07-28 01:38:44 +0100

    Crash Fixed on JobRun

--- TaskWebApplication/TaskWebApplication/Controllers/JobsController.cs
+++ TaskWebApplication/TaskWebApplication/Controllers/JobsController.cs
@@ -170,8 +170,6 @@ namespace TaskWebApplication.Controllers
 		}
 
 
-
-
 // GET: Jobs/ApplyJobParameters/5
 		public ActionResult ApplyJobParameters(int? id)
 		{
@@ -186,71 +184,21 @@ namespace TaskWebApplication.Controllers
 			}
 
 			var taskaControler = new TaskasController();
-
-			var taskaIds = db.Jobs.Include(j => j.CorrespondingTaska)
-										 .Where(item => item.Id == id);
-			var taskaId = taskaIds.ToList().First().TaskaId;
+			var taskaId = db.Jobs.Include(j => j.CorrespondingTaska)
+										 .Where(item => item.Id == id)
+                     .ToList().Last().TaskaId; 
+//			var taskaId = taskaIds.ToList().Last().TaskaId;
 			var subTaskas = taskaControler.GetTaskaSubTaskas(taskaId);
 			var taskaParameters = db.Parameters.Include(p => p.ParameterTaska)
 											 .Where(item => item.TaskaId == taskaId);
 
-			var jobParameters = db.JobParameters.Include(j => j.ParentJob)
-													.Where(item => item.JobId == id);
-
-			List<string> commandList = new List<string>();
-			foreach (SubTaska subtasca in subTaskas)
+      var jobParameters = GetJobParameters(id);
+      foreach (SubTaska subtasca in subTaskas)
 			{
-				var jobResult = new JobResult();
-				jobResult.JobId = (int)id;
-				jobResult.Description = subtasca.Description;
-				jobResult.Command = subtasca.Command;
-				jobResult.ErrorMessage = String.Empty;
-				jobResult.Status = Status.READY;
-				db.JobResults.Add(jobResult);
-				commandList.Add(jobResult.Command);
-			}
-			db.Entry(job).State = EntityState.Modified;
-			db.SaveChanges();
-
-
-			Dictionary<string, string> parametersDictionary = new Dictionary<string, string>();
-			foreach (JobParameters jobpar in jobParameters)
-			{
-				var paramIds = db.Parameters.Include(p => p.JobParameters)
-										 .Where(item => item.Id == jobpar.ParameterId);
-				var paramName = paramIds.ToList().First().ParameterName;
-				parametersDictionary.Add(paramName, jobpar.ParameterValue);
-			}
-
-			List<string> newCommandList = new List<string>();
-
-			foreach (var command in commandList)
-			{
-				string newCommand = command;
-				foreach (Parameter parameter in taskaParameters)
-				{
-					string pattern = @"(\{{2}\s*" + parameter.ParameterName + @"\s*\}{2})";
-					string efectiveValue = "";
-					if (parametersDictionary.ContainsKey(parameter.ParameterName))
-						efectiveValue = parametersDictionary[parameter.ParameterName];
-					else
-						efectiveValue = parameter.ParameterValue;
-					if (Regex.Matches(newCommand, pattern).Count > 0)
-					{
-						newCommand = Regex.Replace(newCommand, pattern, efectiveValue);
-//						System.Diagnostics.Debug.WriteLine(parameter.ParameterName);
-					}
-				}
-				newCommandList.Add(newCommand);
-			}
-
-
-			var jobResults = GetResultsForJob(id);
-			int i = 0;
-			foreach (JobResult jobResult in jobResults)
-			{
-				jobResult.Command = newCommandList[i++];
-//				System.Diagnostics.Debug.WriteLine(jobResult.Command);
+				var jobResult = CreateJobResult(id, subtasca);
+        var newCommand = UpdateCommand(jobResult.Command, taskaParameters, jobParameters);
+			  jobResult.Command = newCommand;
+        db.JobResults.Add(jobResult);
 			}
 
 			job.Status = Status.READY;
@@ -259,7 +207,47 @@ namespace TaskWebApplication.Controllers
 			return null;
 		}
 
-		private IOrderedQueryable<JobResult> GetResultsForJob(int? id)
+	  private static string UpdateCommand(string command, IQueryable<Parameter> taskaParameters, Dictionary<string, string> jobParameters)
+	  {
+	    foreach (Parameter parameter in taskaParameters)
+	    {
+	      string pattern = @"(\{{2}\s*" + parameter.ParameterName + @"\s*\}{2})";
+        string efectiveValue = jobParameters.ContainsKey(parameter.ParameterName) ?
+          jobParameters[parameter.ParameterName] : parameter.ParameterValue;
+	      if (Regex.Matches(command, pattern).Count > 0)
+          command = Regex.Replace(command, pattern, efectiveValue);
+	    }
+	    return command;
+	  }
+
+	  private Dictionary<string, string> GetJobParameters(int? id)
+	  {
+	    var jobParameters = db.JobParameters.Include(j => j.ParentJob)
+	      .Where(item => item.JobId == id);
+
+	    Dictionary<string, string> jobParametersDict = new Dictionary<string, string>();
+	    foreach (JobParameters jobpar in jobParameters)
+	    {
+	      var paramIds = db.Parameters.Include(p => p.JobParameters)
+	        .Where(item => item.Id == jobpar.ParameterId);
+	      var paramName = paramIds.ToList().Last().ParameterName;
+        jobParametersDict.Add(paramName, jobpar.ParameterValue);
+	    }
+	    return jobParametersDict;
+	  }
+
+	  private static JobResult CreateJobResult(int? id, SubTaska subtasca)
+	  {
+	    var jobResult = new JobResult();
+	    jobResult.JobId = (int) id;
+	    jobResult.Description = subtasca.Description;
+	    jobResult.Command = subtasca.Command;
+	    jobResult.ErrorMessage = String.Empty;
+	    jobResult.Status = Status.READY;
+	    return jobResult;
+	  }
+
+	  private IOrderedQueryable<JobResult> GetResultsForJob(int? id)
 		{
 			var jobResults = db.JobResults.Include(j => j.ParentJob)
 				.Where(item => item.JobId == id)
@@ -294,7 +282,7 @@ namespace TaskWebApplication.Controllers
 		}
 
 
-		public Tuple<string, string> RunCommanOnSever(string command)
+		public Tuple<string, string, int> RunCommanOnSever(string command)
 		{
 			Process p = new Process();
 			p.StartInfo.FileName = "cmd.exe";
@@ -304,18 +292,22 @@ namespace TaskWebApplication.Controllers
 			p.StartInfo.UseShellExecute = false;
 			string stdout = String.Empty;
 			string stderr = String.Empty;
-			try
+		  int result = 0;
+
+      try
 			{
 				p.Start();
 				stdout = p.StandardOutput.ReadToEnd();
 				stderr = p.StandardError.ReadToEnd();
 				p.WaitForExit();
-			}
+        result = p.ExitCode;
+      }
 			catch (Exception e)
 			{
-				stdout = e.Message;
-			}
-			return new Tuple<string, string>(stdout, stderr);
+        stderr = e.Message;
+        result = -1;
+      }
+			return new Tuple<string, string, int>(stdout, stderr, result);
 		}
 
 
@@ -338,13 +330,12 @@ namespace TaskWebApplication.Controllers
 			foreach (JobResult jobResult in jobResults)
 			{
 				System.Diagnostics.Debug.Write("Running: " + jobResult.Command + "\n");
-				Tuple<string, string> returned = RunCommanOnSever(jobResult.Command);
+				Tuple<string, string, int> returned = RunCommanOnSever(jobResult.Command);
 				string message = returned.Item1.ToString() + returned.Item2.ToString();
 				jobResult.ErrorMessage = message;
-				System.Diagnostics.Debug.Write(message);
-				if (returned.Item2.ToString() == "" || returned.Item2.ToString().Contains("WARNING") &&
-						!returned.Item2.ToString().Contains("ERROR"))
-					jobResult.Status = Status.SUCCESS;
+//				System.Diagnostics.Debug.Write(message);
+        if (returned.Item3 == 0)
+          jobResult.Status = Status.SUCCESS;
 				else
 				{
 					jobResult.Status = Status.ERROR;
@@ -354,10 +345,7 @@ namespace TaskWebApplication.Controllers
 //				Thread.Sleep(1000);
 			}
 
-			if (error)
-				job.Status = Status.ERROR;
-			else
-				job.Status = Status.SUCCESS;
+			job.Status = error ? Status.ERROR : Status.SUCCESS;
 			db.Entry(job).State = EntityState.Modified;
 			db.SaveChanges();
 			//          return View(await db.Taskas.ToListAsync());
