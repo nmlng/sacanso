00b82420a9b917655023616472a9169b21599e8f
Nuno Guerreiro <nunomguerreiro@gmail.com>
2016-07-28 18:27:07 +0100

    Adicionar e editar subtaskas(Parameters/JobParameters corrigido

--- TaskWebApplication/TaskWebApplication/Controllers/JobsController.cs
+++ TaskWebApplication/TaskWebApplication/Controllers/JobsController.cs
@@ -13,356 +13,356 @@ using TaskWebApplication.Models;
 
 namespace TaskWebApplication.Controllers
 {
-	public class JobsController : Controller
-	{
-		private ApplicationDbContext db = new ApplicationDbContext();
+  public class JobsController : Controller
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
 
-		// GET: Jobs
-		public ActionResult Index()
-		{
-			var jobs = db.Jobs.Include(j => j.CorrespondingTaska)
-				.OrderBy(x => x.JobName)
-				.ThenBy(x => x.CorrespondingTaska);
-			return View(jobs.ToList());
-		}
+    // GET: Jobs
+    public ActionResult Index()
+    {
+      var jobs = db.Jobs.Include(j => j.CorrespondingTaska)
+        .OrderBy(x => x.JobName)
+        .ThenBy(x => x.CorrespondingTaska);
+      return View(jobs.ToList());
+    }
 
-		// GET: Jobs/Details/5
-		public ActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
-			return View(job);
-		}
+    // GET: Jobs/Details/5
+    public ActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
+      return View(job);
+    }
 
-		// GET: Jobs/Create
-		public ActionResult Create()
-		{
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description");
-			return View();
-		}
+    // GET: Jobs/Create
+    public ActionResult Create()
+    {
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description");
+      return View();
+    }
 
-		// POST: Jobs/Create
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Id,JobName,TaskaId")] Job job)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Jobs.Add(job);
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
+    // POST: Jobs/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind(Include = "Id,JobName,TaskaId")] Job job)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Jobs.Add(job);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
 
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
-			return View(job);
-		}
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
+      return View(job);
+    }
 
-		// GET: Jobs/Edit/5
-		public ActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
-			return View(job);
-		}
+    // GET: Jobs/Edit/5
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
+      return View(job);
+    }
 
-		// POST: Jobs/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Id,JobName,TaskaId")] Job job)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Entry(job).State = EntityState.Modified;
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
-			return View(job);
-		}
+    // POST: Jobs/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit([Bind(Include = "Id,JobName,TaskaId")] Job job)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(job).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", job.TaskaId);
+      return View(job);
+    }
 
-		// GET: Jobs/Delete/5
-		public ActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
-			return View(job);
-		}
+    // GET: Jobs/Delete/5
+    public ActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
+      return View(job);
+    }
 
-		// POST: Jobs/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed(int id)
-		{
-			Job job = db.Jobs.Find(id);
-			db.Jobs.Remove(job);
-			db.SaveChanges();
-			return RedirectToAction("Index");
-		}
-
-
-		public ActionResult ResetJob(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
-
-			var jobResults = GetResultsForJob(id);
-
-			//			var subTaskas = db.SubTaskas.SqlQuery("select * from SubTaskas where TaskaId = " + id.ToString());
-			foreach (JobResult jobResult in jobResults)
-			{
-				jobResult.Status = Status.READY;
-				jobResult.ErrorMessage = String.Empty;
-			}
-
-			job.Status = Status.READY;
-			db.Entry(job).State = EntityState.Modified;
-			db.SaveChanges();
-
-			return RedirectToAction("Index");
-		}
+    // POST: Jobs/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Job job = db.Jobs.Find(id);
+      db.Jobs.Remove(job);
+      db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
 
-		public ActionResult ResetAll()
-		{
-			List<Job> jobs = db.Jobs.ToList();
-			
-			foreach (Job job in jobs)
-			{
-				job.Status = Status.READY;
-			}
+    public ActionResult ResetJob(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
 
-			db.SaveChanges();
+      var jobResults = GetResultsForJob(id);
 
-			return RedirectToAction("Index");
-		}
+      //			var subTaskas = db.SubTaskas.SqlQuery("select * from SubTaskas where TaskaId = " + id.ToString());
+      foreach (JobResult jobResult in jobResults)
+      {
+        jobResult.Status = Status.READY;
+        jobResult.ErrorMessage = String.Empty;
+      }
+
+      job.Status = Status.READY;
+      db.Entry(job).State = EntityState.Modified;
+      db.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
 
 
-// GET: Jobs/ApplyJobParameters/5
-		public ActionResult ApplyJobParameters(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
+    public ActionResult ResetAll()
+    {
+      List<Job> jobs = db.Jobs.ToList();
 
-			var taskaControler = new TaskasController();
-			var taskaId = db.Jobs.Include(j => j.CorrespondingTaska)
-										 .Where(item => item.Id == id)
-                     .ToList().Last().TaskaId; 
-//			var taskaId = taskaIds.ToList().Last().TaskaId;
-			var subTaskas = taskaControler.GetTaskaSubTaskas(taskaId);
-			var taskaParameters = db.Parameters.Include(p => p.ParameterTaska)
-											 .Where(item => item.TaskaId == taskaId);
+      foreach (Job job in jobs)
+      {
+        job.Status = Status.READY;
+      }
+
+      db.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
+
+
+    // GET: Jobs/ApplyJobParameters/5
+    public ActionResult ApplyJobParameters(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
+
+      var taskaControler = new TaskasController();
+      var taskaId = db.Jobs.Include(j => j.CorrespondingTaska)
+                     .Where(item => item.Id == id)
+                     .ToList().Last().TaskaId;
+      //			var taskaId = taskaIds.ToList().Last().TaskaId;
+      var subTaskas = taskaControler.GetTaskaSubTaskas(taskaId);
+      var taskaParameters = db.Parameters.Include(p => p.ParameterTaska)
+                       .Where(item => item.TaskaId == taskaId);
 
       var jobParameters = GetJobParameters(id);
       foreach (SubTaska subtasca in subTaskas)
-			{
-				var jobResult = CreateJobResult(id, subtasca);
+      {
+        var jobResult = CreateJobResult(id, subtasca);
         var newCommand = UpdateCommand(jobResult.Command, taskaParameters, jobParameters);
-			  jobResult.Command = newCommand;
+        jobResult.Command = newCommand;
         db.JobResults.Add(jobResult);
-			}
+      }
 
-			job.Status = Status.READY;
-			db.Entry(job).State = EntityState.Modified;
-			db.SaveChanges();
-			return null;
-		}
+      job.Status = Status.READY;
+      db.Entry(job).State = EntityState.Modified;
+      db.SaveChanges();
+      return null;
+    }
 
-	  private static string UpdateCommand(string command, IQueryable<Parameter> taskaParameters, Dictionary<string, string> jobParameters)
-	  {
-	    foreach (Parameter parameter in taskaParameters)
-	    {
-	      string pattern = @"(\{{2}\s*" + parameter.ParameterName + @"\s*\}{2})";
+    private static string UpdateCommand(string command, IQueryable<Parameter> taskaParameters, Dictionary<string, string> jobParameters)
+    {
+      foreach (Parameter parameter in taskaParameters)
+      {
+        string pattern = @"(\{{2}\s*" + parameter.ParameterName + @"\s*\}{2})";
         string efectiveValue = jobParameters.ContainsKey(parameter.ParameterName) ?
           jobParameters[parameter.ParameterName] : parameter.ParameterValue;
-	      if (Regex.Matches(command, pattern).Count > 0)
+        if (Regex.Matches(command, pattern).Count > 0)
           command = Regex.Replace(command, pattern, efectiveValue);
-	    }
-	    return command;
-	  }
+      }
+      return command;
+    }
 
-	  private Dictionary<string, string> GetJobParameters(int? id)
-	  {
-	    var jobParameters = db.JobParameters.Include(j => j.ParentJob)
-	      .Where(item => item.JobId == id);
+    private Dictionary<string, string> GetJobParameters(int? id)
+    {
+      var jobParameters = db.JobParameters.Include(j => j.ParentJob)
+        .Where(item => item.JobId == id);
 
-	    Dictionary<string, string> jobParametersDict = new Dictionary<string, string>();
-	    foreach (JobParameters jobpar in jobParameters)
-	    {
-	      var paramIds = db.Parameters.Include(p => p.JobParameters)
-	        .Where(item => item.Id == jobpar.ParameterId);
-	      var paramName = paramIds.ToList().Last().ParameterName;
+      Dictionary<string, string> jobParametersDict = new Dictionary<string, string>();
+      foreach (JobParameters jobpar in jobParameters)
+      {
+        var paramIds = db.Parameters.Include(p => p.JobParameters)
+          .Where(item => item.Id == jobpar.ParameterId);
+        var paramName = paramIds.ToList().Last().ParameterName;
         jobParametersDict.Add(paramName, jobpar.ParameterValue);
-	    }
-	    return jobParametersDict;
-	  }
+      }
+      return jobParametersDict;
+    }
 
-	  private static JobResult CreateJobResult(int? id, SubTaska subtasca)
-	  {
-	    var jobResult = new JobResult();
-	    jobResult.JobId = (int) id;
-	    jobResult.Description = subtasca.Description;
-	    jobResult.Command = subtasca.Command;
-	    jobResult.ErrorMessage = String.Empty;
-	    jobResult.Status = Status.READY;
-	    return jobResult;
-	  }
+    private static JobResult CreateJobResult(int? id, SubTaska subtasca)
+    {
+      var jobResult = new JobResult();
+      jobResult.JobId = (int)id;
+      jobResult.Description = subtasca.Description;
+      jobResult.Command = subtasca.Command;
+      jobResult.ErrorMessage = String.Empty;
+      jobResult.Status = Status.READY;
+      return jobResult;
+    }
 
-	  private IOrderedQueryable<JobResult> GetResultsForJob(int? id)
-		{
-			var jobResults = db.JobResults.Include(j => j.ParentJob)
-				.Where(item => item.JobId == id)
-				.OrderBy(x => x.JobId)
-				.ThenBy(x => x.Id);
-			return jobResults;
-		}
-
-
-		// GET: Jobs/Run/5
-		public ActionResult RunJob(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
-
-			ApplyJobParameters(id);
-			job.Status = Status.ONGOING;
-			db.Entry(job).State = EntityState.Modified;
-			db.SaveChanges();
-			var result = RunJobCommands(id);
-			ViewData["id"] = job.Id;
-			ViewData["description"] = job.CorrespondingTaska;
-			//          return View(db.Taskas.ToList());
-			return RedirectToAction("Index");
-		}
+    private IOrderedQueryable<JobResult> GetResultsForJob(int? id)
+    {
+      var jobResults = db.JobResults.Include(j => j.ParentJob)
+        .Where(item => item.JobId == id)
+        .OrderBy(x => x.JobId)
+        .ThenBy(x => x.Id);
+      return jobResults;
+    }
 
 
-		public Tuple<string, string, int> RunCommanOnSever(string command)
-		{
-			Process p = new Process();
-			p.StartInfo.FileName = "cmd.exe";
-			p.StartInfo.Arguments = "/C " + command;
-			p.StartInfo.RedirectStandardOutput = true;
-			p.StartInfo.RedirectStandardError = true;
-			p.StartInfo.UseShellExecute = false;
-			string stdout = String.Empty;
-			string stderr = String.Empty;
-		  int result = 0;
+    // GET: Jobs/Run/5
+    public ActionResult RunJob(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
+
+      ApplyJobParameters(id);
+      job.Status = Status.ONGOING;
+      db.Entry(job).State = EntityState.Modified;
+      db.SaveChanges();
+      var result = RunJobCommands(id);
+      ViewData["id"] = job.Id;
+      ViewData["description"] = job.CorrespondingTaska;
+      //          return View(db.Taskas.ToList());
+      return RedirectToAction("Index");
+    }
+
+
+    public Tuple<string, string, int> RunCommanOnSever(string command)
+    {
+      Process p = new Process();
+      p.StartInfo.FileName = "cmd.exe";
+      p.StartInfo.Arguments = "/C " + command;
+      p.StartInfo.RedirectStandardOutput = true;
+      p.StartInfo.RedirectStandardError = true;
+      p.StartInfo.UseShellExecute = false;
+      string stdout = String.Empty;
+      string stderr = String.Empty;
+      int result = 0;
 
       try
-			{
-				p.Start();
-				stdout = p.StandardOutput.ReadToEnd();
-				stderr = p.StandardError.ReadToEnd();
-				p.WaitForExit();
+      {
+        p.Start();
+        stdout = p.StandardOutput.ReadToEnd();
+        stderr = p.StandardError.ReadToEnd();
+        p.WaitForExit();
         result = p.ExitCode;
       }
-			catch (Exception e)
-			{
+      catch (Exception e)
+      {
         stderr = e.Message;
         result = -1;
       }
-			return new Tuple<string, string, int>(stdout, stderr, result);
-		}
+      return new Tuple<string, string, int>(stdout, stderr, result);
+    }
 
 
-		public ActionResult RunJobCommands(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Job job = db.Jobs.Find(id);
-			if (job == null)
-			{
-				return HttpNotFound();
-			}
+    public ActionResult RunJobCommands(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Job job = db.Jobs.Find(id);
+      if (job == null)
+      {
+        return HttpNotFound();
+      }
 
-			var jobResults = GetResultsForJob(id);
+      var jobResults = GetResultsForJob(id);
 
-			//			var subTaskas = db.SubTaskas.SqlQuery("select * from SubTaskas where TaskaId = " + id.ToString());
-			var error = false;
-			foreach (JobResult jobResult in jobResults)
-			{
-				System.Diagnostics.Debug.Write("Running: " + jobResult.Command + "\n");
-				Tuple<string, string, int> returned = RunCommanOnSever(jobResult.Command);
-				string message = returned.Item1.ToString() + returned.Item2.ToString();
-				jobResult.ErrorMessage = message;
-//				System.Diagnostics.Debug.Write(message);
+      //			var subTaskas = db.SubTaskas.SqlQuery("select * from SubTaskas where TaskaId = " + id.ToString());
+      var error = false;
+      foreach (JobResult jobResult in jobResults)
+      {
+        System.Diagnostics.Debug.Write("Running: " + jobResult.Command + "\n");
+        Tuple<string, string, int> returned = RunCommanOnSever(jobResult.Command);
+        string message = returned.Item1.ToString() + returned.Item2.ToString();
+        jobResult.ErrorMessage = message;
+        //				System.Diagnostics.Debug.Write(message);
         if (returned.Item3 == 0)
           jobResult.Status = Status.SUCCESS;
-				else
-				{
-					jobResult.Status = Status.ERROR;
-					error = true;
-					break;
-				}
-//				Thread.Sleep(1000);
-			}
+        else
+        {
+          jobResult.Status = Status.ERROR;
+          error = true;
+          break;
+        }
+        //				Thread.Sleep(1000);
+      }
 
-			job.Status = error ? Status.ERROR : Status.SUCCESS;
-			db.Entry(job).State = EntityState.Modified;
-			db.SaveChanges();
-			//          return View(await db.Taskas.ToListAsync());
-			return RedirectToAction("Index", "Jobs");
-			//          return null;
-		}
-
-
+      job.Status = error ? Status.ERROR : Status.SUCCESS;
+      db.Entry(job).State = EntityState.Modified;
+      db.SaveChanges();
+      //          return View(await db.Taskas.ToListAsync());
+      return RedirectToAction("Index", "Jobs");
+      //          return null;
+    }
 
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				db.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
 }
--- TaskWebApplication/TaskWebApplication/Controllers/ParametersController.cs
+++ TaskWebApplication/TaskWebApplication/Controllers/ParametersController.cs
@@ -6,140 +6,144 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using TaskWebApplication.Models;
 
 namespace TaskWebApplication.Controllers
 {
-	public class ParametersController : Controller
-	{
-		private ApplicationDbContext db = new ApplicationDbContext();
+  public class ParametersController : Controller
+  {
+    private ApplicationDbContext db = new ApplicationDbContext();
 
-		// GET: Parameters
-		public ActionResult Index(int? id)
-		{
-			if (id != null)
-			{
-				var parameters = db.Parameters.Include(p => p.ParameterTaska)
-					.Where(item => item.TaskaId == id)
-					.OrderBy(x => x.TaskaId)
-					.ThenBy(x => x.ParameterName);
-				return View(parameters.ToList());
-			}
-			else
-			{
-				var parameters = db.Parameters.Include(p => p.ParameterTaska)
-					.OrderBy(x => x.TaskaId)
-					.ThenBy(x => x.ParameterName);
-				return View(parameters.ToList());
-			}
-		}
+    // GET: Parameters
+    public ActionResult Index(int? id)
+    {
+      if (id != null)
+      {
+        var parameters = db.Parameters.Include(p => p.ParameterTaska)
+          .Where(item => item.TaskaId == id)
+          .OrderBy(x => x.TaskaId)
+          .ThenBy(x => x.ParameterName);
+        return View(parameters.ToList());
+      }
+      else
+      {
+        var parameters = db.Parameters.Include(p => p.ParameterTaska)
+          .OrderBy(x => x.TaskaId)
+          .ThenBy(x => x.ParameterName);
+        return View(parameters.ToList());
+      }
+    }
 
-		// GET: Parameters/Details/5
-		public ActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Parameter parameter = db.Parameters.Find(id);
-			if (parameter == null)
-			{
-				return HttpNotFound();
-			}
-			return View(parameter);
-		}
+    // GET: Parameters/Details/5
+    public ActionResult Details(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Parameter parameter = db.Parameters.Find(id);
+      if (parameter == null)
+      {
+        return HttpNotFound();
+      }
+      return View(parameter);
+    }
 
-		// GET: Parameters/Create
-		public ActionResult Create(int? TaskaId)
-		{
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", TaskaId);
-			return View();
-		}
+    // GET: Parameters/Create
+    public ActionResult Create(int? TaskaId)
+    {
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", TaskaId);
+      return View();
+    }
 
-		// POST: Parameters/Create
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Create([Bind(Include = "Id,ParameterName,ParameterValue,TaskaId")] Parameter parameter)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Parameters.Add(parameter);
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
+    // POST: Parameters/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create([Bind(Include = "Id,ParameterName,ParameterValue,TaskaId")] Parameter parameter)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Parameters.Add(parameter);
+        db.SaveChanges();
+        return RedirectToAction("Index",
+           new RouteValueDictionary(new { Id = parameter.TaskaId }));
+      }
 
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
-			return View(parameter);
-		}
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
+      return View(parameter);
+    }
 
-		// GET: Parameters/Edit/5
-		public ActionResult Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Parameter parameter = db.Parameters.Find(id);
-			if (parameter == null)
-			{
-				return HttpNotFound();
-			}
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
-			return View(parameter);
-		}
+    // GET: Parameters/Edit/5
+    public ActionResult Edit(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Parameter parameter = db.Parameters.Find(id);
+      if (parameter == null)
+      {
+        return HttpNotFound();
+      }
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
+      return View(parameter);
+    }
 
-		// POST: Parameters/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit([Bind(Include = "Id,ParameterName,ParameterValue,TaskaId")] Parameter parameter)
-		{
-			if (ModelState.IsValid)
-			{
-				db.Entry(parameter).State = EntityState.Modified;
-				db.SaveChanges();
-				return RedirectToAction("Index");
-			}
-			ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
-			return View(parameter);
-		}
+    // POST: Parameters/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit([Bind(Include = "Id,ParameterName,ParameterValue,TaskaId")] Parameter parameter)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(parameter).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index",
+           new RouteValueDictionary(new { Id = parameter.TaskaId }));
+      }
+      ViewBag.TaskaId = new SelectList(db.Taskas, "Id", "Description", parameter.TaskaId);
+      return View(parameter);
+    }
 
-		// GET: Parameters/Delete/5
-		public ActionResult Delete(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			Parameter parameter = db.Parameters.Find(id);
-			if (parameter == null)
-			{
-				return HttpNotFound();
-			}
-			return View(parameter);
-		}
+    // GET: Parameters/Delete/5
+    public ActionResult Delete(int? id)
+    {
+      if (id == null)
+      {
+        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+      }
+      Parameter parameter = db.Parameters.Find(id);
+      if (parameter == null)
+      {
+        return HttpNotFound();
+      }
+      return View(parameter);
+    }
 
-		// POST: Parameters/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public ActionResult DeleteConfirmed(int id)
-		{
-			Parameter parameter = db.Parameters.Find(id);
-			db.Parameters.Remove(parameter);
-			db.SaveChanges();
-			return RedirectToAction("Index");
-		}
+    // POST: Parameters/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Parameter parameter = db.Parameters.Find(id);
+      db.Parameters.Remove(parameter);
+      db.SaveChanges();
+      return RedirectToAction("Index",
+         new RouteValueDictionary(new { Id = parameter.TaskaId }));
+    }
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				db.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+  }
 }
