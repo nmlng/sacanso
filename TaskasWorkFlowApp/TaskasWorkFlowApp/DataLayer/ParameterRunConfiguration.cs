d55125a9c346596232e02354629672e655a0378e
Nuno Guerreiro <nunomguerreiro@gmail.com>
2016-08-19 22:20:59 +0100

    Added Parameter and ParameterRun

---
+++ TaskasWorkFlowApp/TaskasWorkFlowApp/DataLayer/ParameterRunConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using TaskasWorkFlowApp.Models;
+
+namespace TaskasWorkFlowApp.DataLayer
+{
+  public class ParameterRunConfiguration : EntityTypeConfiguration<ParameterRun>
+  {
+    public ParameterRunConfiguration()
+    {
+      Property(m => m.Id).HasColumnName("ParameterRunId");
+
+      HasRequired(m => m.Parameter).WithMany().WillCascadeOnDelete(false);
+      HasRequired(m => m.TaskaRun).WithMany().WillCascadeOnDelete(false);
+
+    }
+  }
+}
--- TaskasWorkFlowApp/TaskasWorkFlowApp/Models/IdentityModels.cs
+++ TaskasWorkFlowApp/TaskasWorkFlowApp/Models/IdentityModels.cs
@@ -7,41 +7,45 @@ using TaskasWorkFlowApp.DataLayer;
 
 namespace TaskasWorkFlowApp.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+  // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+  public class ApplicationUser : IdentityUser
+  {
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
+      // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+      var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+      // Add custom user claims here
+      return userIdentity;
+    }
+  }
+
+  public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext()
+        : base("DefaultConnection", throwIfV1Schema: false)
+    {
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public DbSet<Taska> Taskas { get; set; }
-        public DbSet<TaskaRun> TaskaRuns { get; set; }
-        public DbSet<Note> Notes { get; set; }
+    public DbSet<Taska> Taskas { get; set; }
+    public DbSet<TaskaRun> TaskaRuns { get; set; }
+    public DbSet<Note> Notes { get; set; }
+    public DbSet<Parameter> Parameters { get; set; }
+    public DbSet<ParameterRun> ParameterRuns { get; set; }
 
     protected override void OnModelCreating(DbModelBuilder modelBuilder)
-      {
-        modelBuilder.Configurations.Add(new TaskaConfiguration());
-        modelBuilder.Configurations.Add(new TaskaRunConfiguration());
-        modelBuilder.Configurations.Add(new NoteConfiguration());   
+    {
+      modelBuilder.Configurations.Add(new TaskaConfiguration());
+      modelBuilder.Configurations.Add(new TaskaRunConfiguration());
+      modelBuilder.Configurations.Add(new NoteConfiguration());
+      modelBuilder.Configurations.Add(new ParameterConfiguration());
+      modelBuilder.Configurations.Add(new ParameterRunConfiguration());
 
-        base.OnModelCreating(modelBuilder);
-      }
-
-      public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
+      base.OnModelCreating(modelBuilder);
     }
+
+    public static ApplicationDbContext Create()
+    {
+      return new ApplicationDbContext();
+    }
+  }
 }
