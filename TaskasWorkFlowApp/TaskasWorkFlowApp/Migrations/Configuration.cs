1c58203e4535cfae6989d3227795a01047eec63e
Nuno Guerreiro <nunomguerreiro@gmail.com>
2016-08-25 23:59:35 +0100

    More taskaChild tests

--- TaskasWorkFlowApp/TaskasWorkFlowApp/Migrations/Configuration.cs
+++ TaskasWorkFlowApp/TaskasWorkFlowApp/Migrations/Configuration.cs
@@ -19,22 +19,24 @@ namespace TaskasWorkFlowApp.Migrations
     {
       context.Taskas.AddOrUpdate(
             t => t.TaskaName,
-            new Taska { TaskaName = "Taska1", Description = "Descreve-mos ", },
-            new Taska { TaskaName = "Taska2", Description = "F1  " },
-            new Taska { TaskaName = "Taska3", Description = "F2 " },
-            new Taska { TaskaName = "Taska4", Description = "E1  " },
-            new Taska { TaskaName = "Taska5", Description = "E2 " }
+            new Taska { TaskaName = "TaskaMae1", Description = "Descreve-mos ", },
+            new Taska { TaskaName = "TaskaMae2", Description = "Descreve-mos2 ", },
+            new Taska { TaskaName = "Taska1", Description = "F1  " },
+            new Taska { TaskaName = "Taska2", Description = "F2 " },
+            new Taska { TaskaName = "Taska3", Description = "E1  " },
+            new Taska { TaskaName = "Taska4", Description = "E2 " }
             );
 
       context.SaveChanges();
 
       ICollection<TaskaChild> TaskaChildren = new List<TaskaChild>();
 
-      var taskaMae = context.Taskas.Single(t => t.TaskaName == "Taska1");
-      var taskaFilha1 = context.Taskas.Single(t => t.TaskaName == "Taska2");
-      var taskaFilha2 = context.Taskas.Single(t => t.TaskaName == "Taska3");
-      var taskaFilha3 = context.Taskas.Single(t => t.TaskaName == "Taska4");
-      var taskaFilha4 = context.Taskas.Single(t => t.TaskaName == "Taska5");
+      var taskaMae = context.Taskas.Single(t => t.TaskaName == "TaskaMae1");
+      var taskaMae2 = context.Taskas.Single(t => t.TaskaName == "TaskaMae2");
+      var taskaFilha1 = context.Taskas.Single(t => t.TaskaName == "Taska1");
+      var taskaFilha2 = context.Taskas.Single(t => t.TaskaName == "Taska2");
+      var taskaFilha3 = context.Taskas.Single(t => t.TaskaName == "Taska3");
+      var taskaFilha4 = context.Taskas.Single(t => t.TaskaName == "Taska4");
 
       var childTaska1 = new TaskaChild { Order = 37, ChildTaska = taskaFilha1 };
       TaskaChildren.Add(childTaska1);
@@ -50,27 +52,41 @@ namespace TaskasWorkFlowApp.Migrations
 
       taskaMae.Children = TaskaChildren;
 
-      taskaFilha4.Children = new List<TaskaChild>
+      taskaFilha1.Children = new List<TaskaChild>
       {
         new TaskaChild
         {
           Order = 434,
           ChildTaska = taskaMae
-        }
+        },
+        new TaskaChild
+        {
+          Order = 888,
+          ChildTaska = taskaFilha2
+        },
       };
 
-      taskaFilha3.Children = new List<TaskaChild>
+      taskaFilha2.Children = new List<TaskaChild>
       {
         new TaskaChild
         {
           Order = 777,
-          ChildTaska = taskaFilha2
+          ChildTaska = taskaFilha1
         }
       };
 
-       //     context.Taskas.Remove(taskaMae);
-      // context.TaskaChilds.Remove(childTaska2);
+      context.SaveChanges();
 
+      var filhasXpto = context.TaskaChilds.Where(s => s.ChildTaska.Description.StartsWith("E")).ToList();
+
+      taskaMae2.Children = filhasXpto.Select(
+        taskaChild => new TaskaChild {
+                          Order = taskaChild.Order,
+                          ChildTaska = taskaChild.ChildTaska
+      }).ToList();
+
+      //     context.Taskas.Remove(taskaMae);
+      // context.TaskaChilds.Remove(childTaska2);
     }
   }
 }
